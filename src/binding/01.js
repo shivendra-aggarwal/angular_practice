6bb6f23062417d3406cc8a17b7debb7ff9eeb373
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-03-31 05:59:10 +0530

    added examples of bindings in angularjs

--- src/binding/01.js
+++ src/binding/01.js
@@ -2,4 +2,14 @@ var app = angular.module("app", []);
 app.controller("emp", ["$scope", function($scope){
 	$scope.a = "10";
 	$scope.b = "20";
+
+	$scope.doDoubleAndSum = function(){
+		$scope.a = $scope.a*2;
+		$scope.b = $scope.b*2;
+		$scope.sum = parseInt($scope.a) + parseInt($scope.b);
+	}
+
+	$scope.doSum = function(){
+		$scope.sum2 = parseInt($scope.a) + parseInt($scope.b);
+	}
 }]);
