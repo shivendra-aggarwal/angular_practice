ecc1ca04f0e18f93f79a7e7893dced705dfe926d
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-04-25 23:05:23 +0530

    added another example of using custom directive with  service

--- src/custom-directives/example2/01.js
+++ src/custom-directives/example2/01.js
@@ -1,5 +1,32 @@
 var app = angular.module("app",[]);
 
-app.controller("emp",["$scope",function($scope){
-
+app.controller("emp",["$scope","empService",function($scope,empService){
+	$scope.doSearch = function(){
+		empService.findEmployeeById($scope.searchEmpNo, function(result){
+			$scope.empno = result.empno;
+			$scope.name = result.name;
+			$scope.sal = result.sal;
+			$scope.depno = result.depno;
+			$scope.hireddate = result.hiredDate;
+		});
+	};
 }]);
+
+app.service("empService",["$http","$log",function($http,$log){
+	this.findEmployeeById = function(empId,callBack){
+		$http({
+			url: "http://localhost:50047/api/employees/"+empId,
+			method: "GET"
+		}).then(function(resp){
+			callBack(resp.data);
+		},function(resp){
+			$log.error("Error occured");
+		});
+	};
+}]);
+
+app.directive("empDetails",function(){
+	return{
+		templateUrl: "emp-details.html"
+	};
+});
